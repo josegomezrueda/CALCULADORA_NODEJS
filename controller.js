be9ffe862dcc2508c696dc51e1ea7051ae84cf82
jose.gomez <jose.gomez@bosonit.com>
2021-09-09 13:49:46 +0200

    Modificacion errores

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const moduloOperacion = require("./moduloOperacion");
 
-var id = 1;
+var id = null;
 var operacion;
 var numero;
 
@@ -41,43 +41,50 @@ async function cargarDatos(req, res) {
     if (!numeroRecibido) {
         numeroRecibido = 0;
     }
-    numero = parseInt(numeroRecibido)
-    datos = (req.body)
-    let representacionhis;
-    const operacionCuenta = await moduloOperacion.findById(id);
-    if (operacionCuenta) {
-        contadorOperaciones = calculoOperaciones(operacionCuenta.total, operacion, numero)
-        histOperaciones = operacionCuenta.historico + operacion + numeroRecibido.toString(),
-            await moduloOperacion.findByIdAndUpdate(id, { total: contadorOperaciones, historico: histOperaciones, ultimaMod: Date.now() }, { new: true })
-        if (operacion == 'Reset') {
-            representacionhis = 'VALOR RESETEADO';
-            histOperaciones = '0'
-        } else {
-            representacionhis = 'HISTÓRICO OPERACIONES: ' + histOperaciones + '=' + contadorOperaciones.toString();
-        }
+    if (numeroRecibido == 0 && operacion == '/') {
         res.render('pages/lastOperation', {
-            muestraID: "OPERACION DEL ID: " + id,
-            contOperaciones: "RESULTADOS: " + contadorOperaciones,
-            historicoOperaciones: representacionhis
+            muestraID: "INDETERMINACIÓN, INTENTE OTRA OPERACION",
+            contOperaciones: '',
+            historicoOperaciones: ''
         });
     } else {
-        const nuevaOperacion = new moduloOperacion({ _id: id, total: numeroRecibido, historico: '0+' + numeroRecibido.toString(), ultimaMod: Date.now() });
-        await nuevaOperacion.save();
-        contadorOperaciones = numeroRecibido;
-        representacionhis = 'HISTÓRICO OPERACIONES: 0+' + numeroRecibido.toString() + '=' + contadorOperaciones.toString();
-        res.render('pages/lastOperation', {
-            muestraID: "OPERACION DEL ID: " + id,
-            contOperaciones: "RESULTADOS: " + contadorOperaciones,
-            historicoOperaciones: representacionhis
-        });
+        numero = parseInt(numeroRecibido)
+        datos = (req.body)
+        let representacionhis;
+        const operacionCuenta = await moduloOperacion.findById(id);
+        if (operacionCuenta) {
+            contadorOperaciones = calculoOperaciones(operacionCuenta.total, operacion, numero)
+            histOperaciones = operacionCuenta.historico + operacion + numeroRecibido.toString(),
+                await moduloOperacion.findByIdAndUpdate(id, { total: contadorOperaciones, historico: histOperaciones, ultimaMod: Date.now() }, { new: true })
+            if (operacion == 'Reset') {
+                await moduloOperacion.findByIdAndUpdate(id, { total: 0, historico: '0', ultimaMod: Date.now() }, { new: true })
+                representacionhis = 'VALOR RESETEADO';
+                histOperaciones = '0'
+            } else {
+                representacionhis = 'HISTÓRICO OPERACIONES: ' + histOperaciones + '=' + contadorOperaciones.toString();
+            }
+            res.render('pages/lastOperation', {
+                muestraID: "OPERACION DEL ID: " + id,
+                contOperaciones: "RESULTADO: " + contadorOperaciones,
+                historicoOperaciones: representacionhis
+            });
+        } else {
+            contadorOperaciones = calculoOperaciones(0, operacion, numero)
+            const nuevaOperacion = new moduloOperacion({ _id: id, total: contadorOperaciones, historico: '0' + operacion + (Math.abs(contadorOperaciones)).toString(), ultimaMod: Date.now() });
+            await nuevaOperacion.save();
+            representacionhis = 'HISTÓRICO OPERACIONES: 0' + operacion + numeroRecibido.toString() + '=' + contadorOperaciones.toString();
+            res.render('pages/lastOperation', {
+                muestraID: "OPERACION DEL ID: " + id,
+                contOperaciones: "RESULTADO: " + contadorOperaciones,
+                historicoOperaciones: representacionhis
+            });
+        }
     }
 }
 
 
-
-
 // index page 
-async function paginaPrincipal(req, res){
+async function paginaPrincipal(req, res) {
     res.render('pages/index', {
         idPagina: id
     });
@@ -86,17 +93,18 @@ async function paginaPrincipal(req, res){
 
 // lastOperacion page
 async function paginaResultados(req, res) {
-    if (id) {
-        muestraID = "OPERACION DEL ID: " + id,
-            contOperaciones = 'RESULTADO: ' + contadorOperaciones,
-            historicoOperaciones = 'HISTORICO OPERACIONES: ' + histOperaciones + '=' + contadorOperaciones
-    } else {
+    datosOperaciones = await moduloOperacion.find()
+    if (datosOperaciones.length != 0) {
+        const operacionCuenta = await moduloOperacion.findById(id);
+        muestraID = "OPERACION DEL ID: " + operacionCuenta._id,
+            contOperaciones = 'RESULTADO: ' + operacionCuenta.total,
+            historicoOperaciones = 'HISTORICO OPERACIONES: ' + operacionCuenta.historico + '=' + operacionCuenta.total
+    } else{
         muestraID = 'NO SE HA INTRODUCIDO DATO',
             contOperaciones = 'NO SE HA INICIADO LA CUENTA',
             historicoOperaciones = 'NO HAY HISTORICO DISPONIBLE'
     }
     res.render('pages/lastOperation', {
-
         muestraID: muestraID,
         contOperaciones: contOperaciones,
         historicoOperaciones: historicoOperaciones
@@ -122,7 +130,7 @@ async function myFunc() {
 setInterval(myFunc, 1000);
 
 exports.controller = (app) => {
-    app.post("/form", urlencodedParser, cargarDatos)
+    app.post("/resultado", urlencodedParser, cargarDatos)
     app.get('/', paginaPrincipal)
     app.get('/lastOperation', paginaResultados)
 }
